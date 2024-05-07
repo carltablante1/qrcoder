b2865bcd22b7a19f391fc8de8a11e0ff9e4607bc
Carl Tablante <ctablante@seek.com.au>
2024-05-07 16:15:07 +1000

    decode

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Html5Qrcode } from 'html5-qrcode'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [qrMessage, setQrMessage] = useState('')
+
+  const handleUpload = async (event) => {
+    try {
+      const inputFile = event.target.files[0]
+
+      const html5qrcode = new Html5Qrcode('reader')
+      const decodedMessage = await html5qrcode.scanFile(inputFile, false)
+
+      setQrMessage(decodedMessage)
+    } catch (e) {
+      return "Something went wrong!"
+    }
+  }
 
   return (
     <>
+      <h1>Test</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div id="reader"></div>
+        <input type="file" id="qr-input-file" accept="image/*" onChange={handleUpload} />
+
+        <p>{ qrMessage }</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
